e9d6362da496543e167e4a888deb207d4415e8c8
Gary Fan <tomfan419@gmail.com>
2025-01-02 23:23:14 +0800

    Initial Commit

---
+++ FriendFace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FriendFace
+//
+//  Created by Gary on 2/1/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
